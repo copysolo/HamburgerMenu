114934d5d5c9f477af2b4381b00efdbb368248bc
copysolo <midn@gmx.fr>
2020-05-14 12:25:28 +0200

    Initial Commit

---
+++ HamburgerMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HamburgerMenu
+//
+//  Created by Solo on 14/05/2020.
+//  Copyright © 2020 CopySolo_. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
